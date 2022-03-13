b7ba399580d3317be92dddcc64fcb4683c82e6e6
aigzab <aigzab@yandex.ru>
2022-03-13 17:50:46 +0300

    task_final in progress

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,7 +34,7 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {client: '', order: {}, goods: []};
 
     let countOfGoods = goods.length;
 
@@ -43,9 +43,9 @@ function sendRequest(name, phone, address, goods, sum) {
     }
 
     data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    data.order.sum = sum;
 
-    data.client = 'Иван';
+    data.client = name + phone;
 
     let jsonData = JSON.stringify(data);
 
